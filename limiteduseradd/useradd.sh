8b5ee9cdca42b927075f60d60654d29dbb25cc3c
Igor Pogorelko <ahtyrka@gmail.com>
2019-04-22 13:07:59 +0300

    initiate useradd limited script

---
+++ limiteduseradd/useradd.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# highlights color
+Red='\e[0;31m'
+Green='\e[0;32m'
+Cyan='\e[0;36m'
+Yellow='\e[0;33m'
+NC='\e[0m'
+
+function createuser()
+{
+	# enter username
+	flagEnterUser=0
+	
+	while [[ $flagEnterUser != 1 ]]; do
+		IFEXISTS=0
+		read -p "Please enter the name of user:" NEWUSER
+		
+		#IFEXISTS=$(cat /etc/passwd | awk 'BEGIN{FS=":"}{print $1}' |grep $NEWUSER| wc -l)
+		
+		# check user to exists in /etc/passwd
+		for line in $(cat /etc/passwd | awk 'BEGIN{FS=":"}{print $1}' |grep $NEWUSER); do
+			if [[ $line == $NEWUSER ]]; then
+				IFEXISTS=1
+			fi
+		done
+	
+		if [[ $IFEXISTS == 1 ]]; then
+			echo -e "User ${Yellow} $NEWUSER ${NC} is already exists in /etc/passwd. Please choose another name"
+			continue
+		fi
+	
+		echo -e "new user is ${Green} $NEWUSER ${NC}" 
+		flagEnterUser=1
+	
+	done
+
+	# enter password
+	read -p "Please enter a password for user:" NEWPASSWD
+	
+	# approve changes to the system
+	#useradd -s /bin/bash -p $(openssl passwd -1 ${NEWPASSWD}) -d ${NEWHOMEFOLDER} ${NEWUSER}
+	useradd -s /bin/bash ${NEWUSER}
+	echo ${NEWUSER}:${NEWPASSWD} | chpasswd
+	#mkdir -p ${NEWHOMEFOLDER}
+	#chown ${NEWUSER}:${NEWUSER} ${NEWHOMEFOLDER}
+}
+
+# main functionality
+echo ""
+echo "Please select operations with backup users."
+echo -en "${Cyan}"
+cat << 'EOF'
+Create user (C)
+Delete user (D)
+Update expiration date (U)
+EOF
+echo -en "${NC}"
+#read -p "Please select operations with backup users. Create user(C) Delete user(D) Update user(U) Update quota(Q) Show limits for users(S):" opts
+read -p "Your choose is: " opts
+
+        case "${opts}" in
+                C | c)
+                        echo 'Create user'
+                        createuser
+                        exit
+                ;;
+                D | d)
+                        echo 'Delete user'
+                        #deleteuser
+                        exit
+                ;;
+		U | u)
+		        echo 'Update expiration date'
+			#update_expiration_date
+			exit
+			;;
+                *)
+                        echo "Option is not correct. Please make your choose"
+                ;;
+
+esac
